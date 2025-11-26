b71627f439adb02525a90939b4571e28e6ab1354
easoming <aleksandrlebedev716@gmail.com>
2025-11-26 21:11:19 +0300

    add geometry_lib

---
+++ geometry_lib/circle.py
@@ -0,0 +1,12 @@
+import math
+
+
+def area(r):
+    '''Считает площадь круга радиуса r по формуле pi*r^2'''
+    return math.pi * r * r
+
+
+def perimeter(r):
+    '''Считает длину окружности радиуса r по формуле 2*pi*r'''
+    return 2 * math.pi * r
+
